b07ba93d6e5d917cb493a278ee09c64c52b12ca2
Pandurnag <96949058+Pandurnag@users.noreply.github.com>
2022-07-31 16:33:44 +0530

    SBT-System-Emp

---
+++ src/test/java/com/prog/SbtEmpSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtEmpSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
